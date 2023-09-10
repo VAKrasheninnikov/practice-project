7195874111ace934a504fdecc3c272fcb510bd4f
krasheninnikov_va <krasheninnikov_va@teleformis.ru>
2023-09-10 14:54:28 +0300

    hotfixes

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -32,6 +32,7 @@ export default {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
+    transformIgnorePatterns: ['node_modules/(?!axios)'],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
@@ -169,12 +170,6 @@ export default {
 
     // A map from regular expressions to paths to transformers
     // transform: undefined,
-
-    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    // transformIgnorePatterns: [
-    //   "\\\\node_modules\\\\",
-    //   "\\.pnp\\.[^\\\\]+$"
-    // ],
 
     // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
     // unmockedModulePathPatterns: undefined,
